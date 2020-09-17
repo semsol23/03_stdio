b97cda7a645e0a9aa92260adfd759e9cef003b25
semsol23 <semsol23@naver.com>
2020-09-17 20:49:22 +0900

    4

--- main.c
+++ main.c
@@ -5,14 +5,17 @@
 
 int main(void) {
 	
-	char a;
-	char b;
- 	
- 	printf("enter a character : ");
- 	scanf("%c", &a);
- 	
- 	b = a+1;
- 	printf("The next character of %c (%i) is %c (%i)", a, a, b, b);
+	float a, b, c;
 	
+	printf("enter son : ");
+	scanf("%f", &a);
+	
+	printf("enter mother : ");
+	scanf("%f", &b);
+	
+	c = a/b;
+	
+	printf("the division result is %f", c);
+	 
 	return 0;
 }
